ba71bad16d7482f326271a4e2a61d76f99e667ac
Ayyoob Slamdien <aslamdien90@gmail.com>
2021-09-29 16:25:25 +0200

    Update

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect DataBase
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended:false }));
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the Contact Keeper Api..." })
+);
+
 // Define Routes
-
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper Api...' }))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const port = process.env.port || 5000;
 
